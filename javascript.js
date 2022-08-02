6a26a13c9f281bc85e9026a82e86278e471db3b3
boerbasher <boerbasher@protonmail.com>
2022-08-02 13:15:02 +0100

    ...

--- javascript.js
+++ javascript.js
@@ -33,10 +33,47 @@ function operate(operator, num1, num2) {
     }   
 }
 
-const screen = document.getElementById('screen')
+let ls = ''
 
-const btn = document.querySelectorAll('button');
+let tot = 0
+
+const screen = document.getElementById('screen');
+
+const btn = document.querySelectorAll('.number');
+
+const opBtn = document.querySelectorAll('.operator');
+
+const clearBtn = document.getElementById('clear-button');
+clearBtn.addEventListener('click', clearScreen);
+
+const equalsBtn = document.getElementById('equals');
 
 btn.forEach((button) =>
-    button.addEventListener('click', (e) => screen.createTextNode(e.target.innerHTML)
-))
+  button.addEventListener('click', () => addNumber(button.textContent))
+)
+
+opBtn.forEach((button) =>
+  button.addEventListener('click', () => addOperator(button.textContent))
+)
+
+equalsBtn.addEventListener('click', () => makeList(screen.textContent))
+
+function addNumber(num) {
+    screen.textContent += num
+}
+
+function addOperator(op) {
+    screen.textContent += op
+}
+
+function clearScreen() {
+    screen.textContent = ''
+}
+
+function makeList() {
+    ls = screen.textContent;
+    ls = ls.split(" ");
+    ls = ls.splice();
+    screen.textContent = '';
+    console.log(ls)
+}
